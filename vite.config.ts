e4c65b4c4d35316f773f123b0d461c659fb5f55f
hsabadi <uuhsoabadi@gmail.com>
2025-05-21 22:19:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "./",  // <<-- هنا غيرته لـ "./"
+  base: "xx",  // <<-- هنا غيرته لـ "./"
   plugins: [
     react(),
     mode === "development" && componentTagger(),
